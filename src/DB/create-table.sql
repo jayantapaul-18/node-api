45737098cc56719ce1f3e1c692a4c43cb5d856c8
Jayanta Paul <jayantapaul-18@users.noreply.github.com>
2023-05-28 23:50:36 -0700

    feat: integrate pstgre db for feature flag

---
+++ src/DB/create-table.sql
@@ -0,0 +1,50 @@
+/* create a table called flags with three fields, two VARCHAR types, 
+and an auto-incrementing PRIMARY KEY ID: */
+/* CREATE TABLE  */
+/*#################################################################*/
+CREATE TABLE flags (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  enabled BOOLEAN NOT NULL,
+  project VARCHAR(30),
+  environment VARCHAR(15),
+  description VARCHAR(256),
+  lastToogle DATE,
+  createdAt DATE,
+  updatedAt DATE
+);
+/*#################################################################*/
+/* 
+ */
+/* INSERT INTO  */
+INSERT INTO flags (name, enabled)
+  VALUES ('monitoring', 'true'), ('apm', 'false');
+/*#################################################################*/
+/* SELECT   */
+SELECT * FROM flags;
+SELECT * FROM flags WHERE name = 'api_monitoring';
+SELECT * FROM flags WHERE name = 'api_monitoring' AND enabled = true;
+/*#################################################################*/
+-- Table: public.flags
+
+-- DROP TABLE IF EXISTS public.flags;
+
+CREATE TABLE IF NOT EXISTS public.flags
+(
+    id integer NOT NULL DEFAULT nextval('flags_id_seq'::regclass),
+    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    enabled boolean NOT NULL,
+    project character varying(30) COLLATE pg_catalog."default",
+    environment character varying(15) COLLATE pg_catalog."default",
+    description character varying(256) COLLATE pg_catalog."default",
+    lasttoogle date,
+    createdat date,
+    updatedat date,
+    CONSTRAINT flags_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.flags
+    OWNER to postgres;
+/*#################################################################*/
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,17 +1,17 @@
 /** src/routes/api.ts */
 import express from "express";
 import controller from "../controllers/api";
-import featureAPI from "../controllers/feature-flag";
+// import featureAPI from "../controllers/feature-flag";
 
 const router = express.Router();
 
 router.get("/", controller.defaultGet);
-router.get("/health", controller.getHealth);
+router.all("/health", controller.getHealth);
 router.get("/posts", controller.getPosts);
 router.get("/posts/:id", controller.getPost);
 router.put("/posts/:id", controller.updatePost);
 router.delete("/posts/:id", controller.deletePost);
 router.post("/posts", controller.addPost);
-router.post("/feature-api", featureAPI.featureAPI);
+// router.post("/app/feature-api", featureAPI.featureAPI);
 
 export default router;
